a3fb77e15b9d386a06b196437aa994d7b8f875bb
Dan Crossley <dan@sypo.uk>
2024-11-22 17:08:00 +0000

    Add data grid

--- packages/Sypo/Test/src/DataGrids/TestDataGrid.php
+++ packages/Sypo/Test/src/DataGrids/TestDataGrid.php
@@ -7,5 +7,44 @@ use Illuminate\Support\Facades\DB;
 
 class TestDataGrid extends DataGrid
 {
-    // ...
+    public function prepareQueryBuilder()
+    {
+        $queryBuilder = DB::table('test')
+            ->select('id', 'name', 'created_at', 'updated_at');
+
+        return $queryBuilder;
+    }
+
+    public function prepareColumns()
+    {
+        $this->addColumn([
+            'index'              => 'id',
+            'label'              => 'ID',
+            'type'               => 'string',
+            'searchable'         => true,
+            'filterable'         => false,
+            'sortable'   => true,
+        ]);
+        $this->addColumn([
+            'index'              => 'name',
+            'label'              => 'Name',
+            'type'               => 'string',
+            'searchable'         => true,
+            'filterable'         => false,
+            'sortable'   => true,
+        ]);
+    }
+
+    public function prepareActions()
+    {
+        $this->addAction([
+            'icon'   => 'icon-edit',
+            'title'  => trans('blog::app.admin.datagrid.edit'),
+            'method' => 'GET',
+            'url'    => function ($row) {
+                return '#';
+                #return route('admin.test.edit', $row->id);
+            },
+        ]);
+    }
 }
